6e9543a79bd0ad2dbd909c2961a31abd5cd47d76
unknown <kr.ashwini1123@gmail.com>
2024-01-30 19:33:59 +0530

    final-commit

--- color-flipper/script.js
+++ color-flipper/script.js
@@ -1,16 +1,30 @@
 'use strict';
-const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+// const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+const hexColors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
 const color = document.querySelector('.color');
 const btn = document.querySelector('.btn');
 
-btn.addEventListener('click', function () {
-  const random = generateRandom();
-  console.log(random);
+// btn.addEventListener('click', function () {
+//   const random = generateRandom();
+//   console.log(random);
 
-  document.body.style.backgroundColor = colors[random];
-  color.textContent = colors[random];
+//   document.body.style.backgroundColor = colors[random];
+//   color.textContent = colors[random];
+// });
+
+// dynamically adding color
+btn.addEventListener('click', function () {
+  let hexCode = '#';
+  for (let i = 0; i < 6; i++) {
+    hexCode += hexColors[generateRandom()];
+  }
+  document.body.style.backgroundColor = hexCode;
+  color.textContent = hexCode;
 });
+
+// for generateing random number
+
 function generateRandom() {
-  return Math.floor(Math.random() * colors.length);
+  return Math.floor(Math.random() * hexColors.length);
 }
