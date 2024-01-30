4129d1220ab006e835f30d957887cfd2cdd9bcc4
unknown <kr.ashwini1123@gmail.com>
2024-01-30 18:27:56 +0530

    reset all

--- color-flipper/script.js
+++ color-flipper/script.js
@@ -3,11 +3,3 @@
 const container = document.getElementById('container');
 const heading = document.querySelector('.heading');
 const btn = document.querySelector('.btn');
-
-// on click it will get into darkmode
-btn.addEventListener('click', function () {
-  heading.style.color = '#fff';
-  container.style.backgroundColor = '#222';
-  btn.style.color = '#222';
-  btn.style.backgroundColor = '#fff';
-});
