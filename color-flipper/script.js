4dae0624bb845ed1e7be68724a091f570b7d150d
unknown <kr.ashwini1123@gmail.com>
2024-01-30 19:04:44 +0530

    add click event in  button for changing background color

--- color-flipper/script.js
+++ color-flipper/script.js
@@ -1,5 +1,17 @@
 'use strict';
-
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
 const container = document.getElementById('container');
 const heading = document.querySelector('.heading');
+const color = document.querySelector('.color');
 const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function () {
+  const random = generateRandom();
+  console.log(random);
+
+  document.body.style.backgroundColor = colors[random];
+  color.textContent = colors[random];
+});
+function generateRandom() {
+  return Math.floor(Math.random() * colors.length);
+}
